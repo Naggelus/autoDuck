315ec58bec1737983ded8c364f4bfa1c7a372e8d
Naggelus <foreyoue@gmail.com>
2021-02-03 14:00:58 +0100

    update

--- autoDuck.bat
+++ autoDuck.bat
@@ -1,5 +1,5 @@
 @echo off
 if NOT EXIST "%temp%\autoDuck.ps1" (
-powershell -noprofile -Command "iwr https://raw.githubusercontent.com/Naggelus/autoDuck/master/autoDuck.ps1 -OutFile $env:TEMP\autoDuck.ps1"
+powershell -noprofile -Command "iwr https://raw.githubusercontent.com/Naggelus/autoDuck/master/autoDuck.ps1 -OutFile %temp%\autoDuck.ps1"
 )
-powershell -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""$env:TEMP\autoDuck.ps1""' -Verb RunAs}"
+powershell -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%temp%\autoDuck.ps1""' -Verb RunAs}"
