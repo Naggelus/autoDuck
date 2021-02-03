56e9b4aa284a0ea6c83f32af3f5d3de3e09f9f70
Naggelus <foreyoue@gmail.com>
2021-02-03 15:09:50 +0100

    fixed my stupidness

--- autoDuck.bat
+++ autoDuck.bat
@@ -1,5 +1,6 @@
 @echo off
-if NOT EXIST "%temp%\autoDuck.ps1" (
-powershell -noprofile -Command "iwr https://raw.githubusercontent.com/Naggelus/autoDuck/master/autoDuck.ps1 -OutFile %temp%\autoDuck.ps1"
+if EXIST "%temp%\autoDuck.ps1" (
+del %temp%\autoDuck.ps1
 )
+powershell -noprofile -Command "iwr https://raw.githubusercontent.com/Naggelus/autoDuck/master/autoDuck.ps1 -OutFile %temp%\autoDuck.ps1"
 powershell -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%temp%\autoDuck.ps1""' -Verb RunAs}"
